c8c6fc4cbda11d25e301ea20d668effec58adc7d
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-06-27 19:05:26 +0500

    update model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Contact(models.Model):
+    title = models.CharField(max_length=30, null=False, blank=False)
+    description = models.TextField()
+    work_time = models.CharField(max_length=20, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Service(models.Model):
+    name = models.CharField(max_length=20)
+    price = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
+
+
+class Employee(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone_number = models.CharField(max_length=13, unique=True)
+    image = models.ImageField(upload_to='employee/')
+
+    def __str__(self):
+        return self.first_name
