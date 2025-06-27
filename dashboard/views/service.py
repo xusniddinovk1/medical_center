4389497af23c18c8572f360f5c5929592dc72df8
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-06-27 22:38:57 +0500

    update views

---
+++ dashboard/views/service.py
@@ -0,0 +1,55 @@
+from django.shortcuts import render, redirect
+from core.models import Service
+from dashboard.forms import ServiceForm
+from dashboard.views.home import login_required_decorator
+
+
+@login_required_decorator
+def service_list(request):
+    services = Service.objects.all()
+    ctx = {
+        'service': services
+    }
+    return render(request, 'dashboard/service/list.html', ctx)
+
+
+@login_required_decorator
+def create_service(request):
+    model = Service()
+    form = ServiceForm(request.POST or None, instance=model)
+    if request.POST and form.is_valid():
+        form.save()
+        return redirect('service_list')
+    ctx = {
+        'model': model,
+        'form': form
+    }
+    return render(request, 'dashboard/service/form.html', ctx)
+
+
+@login_required_decorator
+def edit_service(request, pk):
+    model = Service.objects.get(pk=pk)
+    form = ServiceForm(request.POST or None, instance=model)
+    if request.POST and form.is_valid():
+        form.save()
+        return redirect('service_list')
+    ctx = {
+        'model': model,
+        'form': form
+    }
+    return render(request, 'dashboard/service/form.html', ctx)
+
+
+@login_required_decorator
+def delete_service(request, pk):
+    model = Service.objects.get(pk=pk)
+    form = ServiceForm(request.POST or None, instance=model)
+    if request.POST and form.is_valid():
+        form.save()
+        return redirect('service_list')
+    ctx = {
+        'model': model,
+        'form': form
+    }
+    return render(request, 'dashboard/service/form.html', ctx)
